759c7564b05c3c56187a4e4f4a09b4b3ae34d4d9
Dorlig <asger@kragmorild.dk>
2024-12-12 13:53:48 +0100

    Updated images

--- recipe.js
+++ recipe.js
@@ -1,7 +1,7 @@
 // All recipes
 
 let recipes = {
-    "Spaghetti Bolognese": {"timeTotal": 60, "time": 15, "difficulty": "#ffd300", "health": "#ffd300", "image": "/kunst.jpg", ingredients: {
+    "Spaghetti Bolognese": {"timeTotal": 60, "time": 15, "difficulty": "#ffd300", "health": "#ffd300", "image": "kunst.jpg", ingredients: {
         "bacon": "50 g",
         "løg": "2 stk",
         "hvidløg": "2 stk",
@@ -14,14 +14,14 @@ let recipes = {
         "pasta": "400 g",
         "parmesan": "75 g"
     }},
-    "Laks med Rodfrugter": {"timeTotal": 60, "time": 15, "difficulty": "#ffd300", "health": "green", "image": "/kunst.jpg", ingredients: {
+    "Laks med Rodfrugter": {"timeTotal": 60, "time": 15, "difficulty": "#ffd300", "health": "green", "image": "kunst.jpg", ingredients: {
         "laks": "500 g",
         "kartoffel": "540 g",
         "rodfrugter": "1000 g",
         "løg": "2 stk",
         "olie": "4 spsk"
     }},
-    "Nachos": {"timeTotal": 25, "time": 10, "difficulty": "green", "health": "red", "image": "/kunst.jpg", ingredients: {
+    "Nachos": {"timeTotal": 25, "time": 10, "difficulty": "green", "health": "red", "image": "pandekager.jpg", ingredients: {
         "nachos": "125 g",
         "oliven": "150 g",
         "cheddar": "200 g",
@@ -31,14 +31,14 @@ let recipes = {
         "tomat": "10 små stk",
         "majs": "1 dåse"
     }},
-    "Avocado Sandwich": {"timeTotal": 25, "time": 10, "difficulty": "green", "health": "red", "image": "/kunst.jpg", ingredients: {
+    "Avocado Sandwich": {"timeTotal": 25, "time": 10, "difficulty": "green", "health": "red", "image": "kunst.jpg", ingredients: {
         "avocado": "1 stk",
         "lime": "1 stk",
         "smør": "1 tsk",
         "brød": "2 skiver",
         "ost": "2 skiver"
     }},
-    "Pandekager": {"timeTotal": 60, "time": 60, "difficulty": "#ffd300", "health": "red", "image": "/pandekager.jpg", ingredients: {
+    "Pandekager": {"timeTotal": 60, "time": 60, "difficulty": "#ffd300", "health": "red", "image": "pandekager.jpg", ingredients: {
         "hvedemel": "500 g",
         "mælk": "2 dl",
         "øl": "1 dåse",
